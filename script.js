4be51fdcd32166a98d26ab95b1ca33376f34344f
Bojan Popovski <144334411+Booyan15@users.noreply.github.com>
2025-04-15 22:57:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const busSchedule = [
   { bus: 'ТАСИМ КОМПАНИ', station: 'ОРИЗАРИ', area: 'РЕКА', time: '19:30' },
   { bus: '102', station: 'АТОМСКИ', area: 'Руен', time: '16:00' },
   { bus: '103', station: 'УСОВА ЧЕШМА', area: 'Такси Ројал', time: '20:15' },
-  { bus: '104', station: 'АВТОБУСКА', area: 'Пазарче', time: '22:45' },
+  { bus: '104', station: 'АВТОБУСКА', area: 'Пазарче', time: '23:45' },
   { bus: '105', station: 'ПОШТА', area: 'Пошта', time: '20:45' },
   { bus: '106', station: 'ХОТЕЛ ЛЕДЕР', area: 'Хотел Ледер', time: '22:50' },
   { bus: '106', station: 'ПЛОШТАД', area: 'Собрание', time: '20:15' },
